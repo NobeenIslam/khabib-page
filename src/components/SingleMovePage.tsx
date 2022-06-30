61b5d0de5df68dea30664d96e0d3c91116429c7b
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-07-01 00:59:47 +0100

    Styled Moves

--- src/components/SingleMovePage.tsx
+++ src/components/SingleMovePage.tsx
@@ -6,20 +6,19 @@ interface SingleMovePageProps {
 
 export function SingleMovePage({ move }: SingleMovePageProps): JSX.Element {
   return (
-    <section>
-      <h1>{move.title}</h1>
-      <p>{move.description}</p>
-      <div className="d-flex flex-column align-items-center">
-        <iframe
-          width="560"
-          height="315"
-          src={move.youtubeUrl}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+    <section className="d-flex flex-column justify-content-evenly align-items-center">
+      <h1 className="m-3">{move.title}</h1>
+      <p className="move--text text-center">{move.description}</p>
+      <iframe
+        className="move--video"
+        width="560"
+        height="315"
+        src={move.youtubeUrl}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </section>
   );
 }
