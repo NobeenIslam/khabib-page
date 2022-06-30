f960e4ab5b1078d9762a8dfeed9d41f8e87e673e
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-30 01:01:14 +0100

    Conditional page rendering on NavBar Click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import { About } from "./components/About";
 import { Footer } from "./components/Footer";
+import { FormidableOpponents } from "./components/FormidableOpponents";
 import { Home } from "./components/Home";
+import { MovesList } from "./components/MovesList";
 import { NavBar } from "./components/NavBar";
 
 function App(): JSX.Element {
+  const [pageView, setPageView] = useState<string>("Home");
+
   return (
     <main>
-      <NavBar />
-      <Home />
+      <NavBar setPageView={setPageView} />
+      {pageView === "Home" && <Home />}
+      {pageView === "About" && <About />}
+      {pageView === "Moves-List" && <MovesList />}
+      {pageView === "Formidable-Opponents" && <FormidableOpponents />}
       <Footer />
     </main>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,28 @@
-export function NavBar(): JSX.Element {
+interface NavBarProps {
+  setPageView: (arg0: string) => void;
+}
+
+export function NavBar({ setPageView }: NavBarProps): JSX.Element {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
         <div className="navbar-nav">
-          <button className="nav-link btn customFont">About</button>
-          <button className="nav-link btn customFont">Moves List</button>
-          <button className="nav-link btn customFont">
+          <button
+            className="nav-link btn customFont"
+            onClick={() => setPageView("About")}
+          >
+            About
+          </button>
+          <button
+            className="nav-link btn customFont"
+            onClick={() => setPageView("Moves-List")}
+          >
+            Moves List
+          </button>
+          <button
+            className="nav-link btn customFont"
+            onClick={() => setPageView("Formidable-Opponents")}
+          >
             {" "}
             Formidable Opponents
           </button>
@@ -14,6 +31,7 @@ export function NavBar(): JSX.Element {
           src="images/eagle.png"
           alt=""
           className="navbar-brand nav--logo btn"
+          onClick={() => setPageView("Home")}
         />
       </div>
     </nav>
