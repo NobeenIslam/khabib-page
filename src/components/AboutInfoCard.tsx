313a320ea96acfbbef64253a8307903b851c9370
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-30 01:01:38 +0100

    Structuring about page

--- src/components/AboutInfoCard.tsx
+++ src/components/AboutInfoCard.tsx
@@ -1,3 +1,15 @@
-export function AboutInfoCard(): JSX.Element {
-  return <></>;
+import { AboutDataInterface } from "../utils/interfaces";
+
+interface AboutInfoCardProps {
+  data: AboutDataInterface;
+}
+
+export function AboutInfoCard({ data }: AboutInfoCardProps): JSX.Element {
+  return (
+    <section>
+      <h2>{data.title}</h2>
+      <p>{data.text}</p>
+      <img src={data.image} alt="" />
+    </section>
+  );
 }
